831218484b575b597893b1ec5bd21b38436b385e
Sandip Roy <sunzukzkun@gmail.com>
2024-04-20 16:13:07 +0530

    Added Dummy emails and class for testing purpose

--- db/bgp_database.sql
+++ db/bgp_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: Mar 16, 2024 at 02:40 AM
+-- Generation Time: Apr 20, 2024 at 10:41 AM
 -- Server version: 8.3.0
 -- PHP Version: 8.2.8
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `adminx` (
-  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -48,14 +48,35 @@ INSERT INTO `adminx` (`username`, `password`, `id`) VALUES
 
 CREATE TABLE `classes` (
   `id` int NOT NULL,
-  `topic` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `teacher` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` text COLLATE utf8mb4_general_ci NOT NULL,
-  `DEPT` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
+  `topic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `teacher` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `DEPT` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `start_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `end_time` datetime NOT NULL,
-  `notes` longtext COLLATE utf8mb4_general_ci
+  `notes` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `classes`
+--
+
+INSERT INTO `classes` (`id`, `topic`, `teacher`, `description`, `DEPT`, `start_time`, `end_time`, `notes`) VALUES
+(1, 'Final Year Major Project ', 'Sanat Ghosh', 'In this class we are going to discuss about the final year project of  CST dept. ', 'CST', '2024-04-21 10:00:00', '2024-04-21 11:30:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `CST _class_1`
+--
+
+CREATE TABLE `CST _class_1` (
+  `id` int NOT NULL,
+  `dept` varchar(50) NOT NULL,
+  `reg_no` varchar(50) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `status_attend` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -65,14 +86,22 @@ CREATE TABLE `classes` (
 
 CREATE TABLE `student_info` (
   `id` int NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(9999) COLLATE utf8mb4_general_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `semester` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
-  `department` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `registration_number` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(9999) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `semester` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `department` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `registration_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `photos` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_info`
+--
+
+INSERT INTO `student_info` (`id`, `email`, `password`, `name`, `semester`, `department`, `registration_number`, `photos`) VALUES
+(22, 'dummy@gmail.com', '945d907f6a5e50d7f95b96925dca6a32e09eb782060956e3f122e24d7f90f8da', 'Dummy Name', '6', 'CST', 'DUMMYREGID', NULL),
+(23, 'lorem@lorem.com', '8e5464a9c2c12aee29b241848468bc2006976bb79623d45c2f5539cfc22908b9', 'Lorem', '6', 'CFS', 'LOREMEPTID', NULL);
 
 --
 -- Indexes for dumped tables
@@ -88,6 +117,12 @@ ALTER TABLE `adminx`
 -- Indexes for table `classes`
 --
 ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `CST _class_1`
+--
+ALTER TABLE `CST _class_1`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -112,13 +147,19 @@ ALTER TABLE `adminx`
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `CST _class_1`
+--
+ALTER TABLE `CST _class_1`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `student_info`
 --
 ALTER TABLE `student_info`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
