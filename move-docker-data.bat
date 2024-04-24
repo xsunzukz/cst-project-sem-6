1d3683eaa9afe9ab03216e093cf3b4d14ee83444
Sandip Roy <143884822+xsunzukz@users.noreply.github.com>
2024-04-24 08:01:53 +0530

    Update move-docker-data.bat

--- move-docker-data.bat
+++ move-docker-data.bat
@@ -8,5 +8,5 @@ wsl -l -v
 wsl --export docker-desktop-data D:\docker-data\dockerdesktop.tar
 wsl --unregister docker-desktop-data
 wsl -l -v
-wsl --import docker-desktop-data D:\dockr-data\desktop D:\docker-data\dockerdesktop.tar
+wsl --import docker-desktop-data D:\docker-data\desktop D:\docker-data\dockerdesktop.tar
 wsl -l -v
